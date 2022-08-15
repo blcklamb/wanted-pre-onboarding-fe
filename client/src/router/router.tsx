5ab2a53ef6d279e240cf4dcd71354f701f4a1c6d
chaejung Kim <whenucan35@gmail.com>
2022-08-15 23:24:28 +0900

    Set: basic_ router and app.tsx

---
+++ client/src/router/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+function Router() {
+  return (
+    <>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default Router;
